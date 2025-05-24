be64ed5f1e19dafa75411bca9b4a3c339a217ff7
JagdishPathakji <jagdishgithub1@gmail.com>
2025-05-24 17:11:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -1,8 +1,35 @@
 class Solution {
   public:
     // Function to find sum of weights of edges of the Minimum Spanning Tree.
+    int findparent(int u, vector<int> &parent) {
+        
+        if(u == parent[u])
+        return u;
+        
+        // path compression
+        return parent[u] = findparent(parent[u],parent);
+    }
+    
+    void unionbyrank(int u, int v, vector<int> &parent, vector<int> &rank) {
+        
+        int pu = findparent(u,parent);
+        int pv = findparent(v,parent);
+        
+        if(rank[pu] > rank[pv]) {
+            parent[pv] = pu;
+        }
+        else if(rank[pv] > rank[pu]) {
+            parent[pu] = pv;
+        }
+        else {
+            parent[pv] = pu;
+            rank[pu]++;
+        }
+    }
+    
     int spanningTree(int V, vector<vector<int>> adj[]) {
-        // code here
+    
+        /* Prims 
         vector<bool> isMST(V,false);
         vector<int> parent(V,-1);
         int cost = 0;
@@ -33,5 +60,49 @@ class Solution {
         }
         
         return cost;
+        */
+        
+        // Ultimate parent will help us to determine the set
+        vector<int> parent(V);
+        // It will help us to merge the set
+        vector<int> rank(V,0);
+        
+        for(int i=0; i<V; i++) {
+            parent[i] = i;
+        }
+        
+        // Priority Queue
+        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> pq;
+        for(int i=0; i<V; i++) {
+            for(int j=0; j<adj[i].size(); j++) {
+                pq.push({adj[i][j][1],{i,adj[i][j][0]}});
+            }
+        }
+        
+        int cost = 0;
+        int edges = 0;
+        while(!pq.empty()) {
+            auto top = pq.top();
+            pq.pop();
+            
+            int wt = top.first;
+            int u = top.second.first;
+            int v = top.second.second;
+            
+            // check if u and v are in same set or different set
+            if(findparent(u,parent) != findparent(v,parent)) {
+                cost += wt;
+                unionbyrank(u,v,parent,rank);
+                edges++;
+            }
+            
+            //
+            if(edges == V-1)
+            break;
+            
+        }
+
+        return cost;        
+        
     }
 };
