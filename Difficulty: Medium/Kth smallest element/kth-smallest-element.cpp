78c7109e1395b5b535ad25d186d57467938c05c3
JagdishPathakji <jagdishgithub1@gmail.com>
2025-05-30 14:37:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kth smallest element/kth-smallest-element.cpp
+++ Difficulty: Medium/Kth smallest element/kth-smallest-element.cpp
@@ -1,9 +1,7 @@
-
 class Solution {
   public:
-    // Function to find the kth smallest element in the array.
     int kthSmallest(vector<int>& arr, int k) {
-        // Your code here
+        // code here
         priority_queue<int, vector<int>, greater<int>> pq(arr.begin(),arr.end());
         
         while(k-- and !pq.empty()) {
