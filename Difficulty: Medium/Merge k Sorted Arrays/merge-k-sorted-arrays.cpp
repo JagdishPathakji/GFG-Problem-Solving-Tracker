29253a8fefc47f04df0cef18d6ee727495574b28
JagdishPathakji <jagdishgithub1@gmail.com>
2025-05-30 14:34:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge k Sorted Arrays/merge-k-sorted-arrays.cpp
+++ Difficulty: Medium/Merge k Sorted Arrays/merge-k-sorted-arrays.cpp
@@ -1,21 +1,32 @@
+// User function Template for C++
+
 class Solution {
   public:
     // Function to merge k sorted arrays.
-    vector<int> mergeKArrays(vector<vector<int>> arr, int K) {
+    vector<int> mergeKArrays(vector<vector<int>>& arr, int K) {
         // code here
         vector<int> ans;
-        priority_queue<int, vector<int>, greater<int>> pq;
+        
+        vector<pair<int,pair<int,int>>> temp;
         for(int i=0; i<arr.size(); i++) {
-            for(int j=0; j<arr[i].size(); j++) {
-                pq.push(arr[i][j]);
-            }
+            temp.push_back({arr[i][0],{i,0}});
         }
+        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> pq(temp.begin(),temp.end());
         
         while(!pq.empty()) {
-            ans.push_back(pq.top());
+            
+            int val = pq.top().first;
+            int i = pq.top().second.first;
+            int j = pq.top().second.second;
             pq.pop();
+            
+            ans.push_back(val);
+            
+            if(j+1 < arr[i].size())
+            pq.push({arr[i][j+1],{i,j+1}});
         }
         
         return ans;
+    
     }
 };
