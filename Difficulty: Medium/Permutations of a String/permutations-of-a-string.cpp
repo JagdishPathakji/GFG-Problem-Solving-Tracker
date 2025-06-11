099ccab54b1d1488d095534f1ec51a071dcbe475
Jagdish Pathakji <jagdishgithub1@gmail.com>
2025-06-11 21:55:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Permutations of a String/permutations-of-a-string.cpp
+++ Difficulty: Medium/Permutations of a String/permutations-of-a-string.cpp
@@ -1,28 +1,31 @@
 class Solution {
   public:
-    
-    void find(string &s, set<string> &ans, int index) {
+  
+    void find(string &s, vector<string> &ans, int index) {
         
-        if(index == s.size()) {
-            ans.insert(s);
+        if(index >= s.size()) {
+            ans.push_back(s);
             return;
         }
         
+        vector<bool> used(26,0);
         for(int i=index; i<s.size(); i++) {
-            swap(s[index],s[i]);
-            find(s,ans,index+1);
-            swap(s[index],s[i]);
+            if(!used[s[i]-'A']) {
+                used[s[i]-'A'] = 1;
+                swap(s[i],s[index]);
+                find(s,ans,index+1);
+                swap(s[i],s[index]);
+            }
         }
     }
   
     vector<string> findPermutation(string &s) {
-
-        set<string> ans;
+        
+        vector<string> ans;
         int index = 0;
         
         find(s,ans,index);
         
-        vector<string> realans(ans.begin(),ans.end());
-        return realans;
+        return ans;
     }
 };
