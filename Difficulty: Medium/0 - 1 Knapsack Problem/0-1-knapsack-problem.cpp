179ab0babab71e29d26e2d0d17863190d6c0e23d
Jagdish Pathakji <jagdishgithub1@gmail.com>
2025-06-18 21:35:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -1,34 +1,18 @@
 class Solution {
   public:
     
-    int find(int index, int capacity, vector<int> &val, vector<int> &wt, vector<vector<int>> &dp) {
-        
-        if(index <= 0)
-        return 0;
-        
-        if(dp[index][capacity] != -1)
-        return dp[index][capacity];
-        
-        if(capacity < wt[index-1])
-        return dp[index][capacity] = find(index-1,capacity,val,wt,dp);
-        else
-        return dp[index][capacity] = max(val[index-1] + find(index-1,capacity-wt[index-1],val,wt,dp),find(index-1,capacity,val,wt,dp));
-    }
-    
     int knapsack(int W, vector<int> &val, vector<int> &wt) {
         
         int n = val.size();
-        vector<vector<int>>dp(n+1,vector<int>(W+1,0));
+        vector<int> dp(W+1,0);
         
         for(int i=1; i<=n; i++) {
-            for(int j=1; j<=W; j++) {
-                if(j < wt[i-1])
-                dp[i][j] = dp[i-1][j];
-                else
-                dp[i][j] = max(val[i-1]+dp[i-1][j-wt[i-1]],dp[i-1][j]);
+            for(int j=W; j>=1; j--) {
+                if(j >= wt[i-1])
+                dp[j] = max(dp[j],val[i-1]+dp[j-wt[i-1]]);
             }
         }
         
-        return dp[n][W];
+        return dp[W];
     }
 };
