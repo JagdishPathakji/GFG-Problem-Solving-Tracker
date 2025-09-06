57ab5b6f947012ee11e5d6e028fc22ac1ec7beaa
Jagdish Pathakji <jagdishgithub1@gmail.com>
2025-09-06 10:01:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Stickler Thief/stickler-thief.cpp
+++ Difficulty: Medium/Stickler Thief/stickler-thief.cpp
@@ -1,26 +1,22 @@
 class Solution {
   public:
     
-    int find(int index ,vector<int> &arr, vector<int> &dp) {
+    int find(int index, vector<int> &arr, vector<int> &dp) {
         
-        if(index >= arr.size()) {
-            return 0;
-        }
+        if(index >= arr.size()) return 0;
         
         if(dp[index] != -1)
         return dp[index];
         
-        int steal = arr[index] + find(index+2,arr,dp);
-        int skip = find(index+1,arr,dp);
+        int taken = arr[index] + find(index+2,arr,dp);
+        int left = find(index+1,arr,dp);
         
-        return dp[index] = max(steal,skip);
-        
+        return dp[index] = max(taken,left);
     }
-    
+  
     int findMaxSum(vector<int>& arr) {
         
-        int n = arr.size();
-        vector<int> dp(n+1,-1);
+        vector<int> dp(arr.size()+1,-1);
         return find(0,arr,dp);
     }
 };
