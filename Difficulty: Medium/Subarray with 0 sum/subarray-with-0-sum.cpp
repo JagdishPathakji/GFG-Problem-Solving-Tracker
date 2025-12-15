9eaec4ed4444e631a81cd7e1dd9f6570f7f8e3fd
Jagdish Pathakji <jagdishgithub1@gmail.com>
2025-12-15 11:01:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subarray with 0 sum/subarray-with-0-sum.cpp
+++ Difficulty: Medium/Subarray with 0 sum/subarray-with-0-sum.cpp
@@ -1,21 +1,20 @@
 class Solution {
   public:
-    
+    // Complete this function
+    // Function to check whether there is a subarray present with 0-sum or not.
     bool subArrayExists(vector<int>& arr) {
         
+        int prefixSum = 0;
         unordered_map<int,bool> mp;
-        int prefixSum = arr[0];
-        mp[0] = 1;
-        mp[prefixSum] = 1;
         
-        if(prefixSum == 0)
-        return true;
-        
-        for(int i=1; i<arr.size(); i++) {
+        for(int i=0; i<arr.size(); i++) {
             
             prefixSum += arr[i];
             
-            if(mp[prefixSum])
+            if(prefixSum == 0)
+            return true;
+            
+            if(mp[prefixSum] == true)
             return true;
             
             mp[prefixSum] = true;
