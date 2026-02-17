b4d781fc1cb634ba2862f6b153b0a475f448bb82
Jagdish Pathakji <jagdishgithub1@gmail.com>
2026-02-17 17:25:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Is Binary Tree Heap/is-binary-tree-heap.cpp
+++ Difficulty: Medium/Is Binary Tree Heap/is-binary-tree-heap.cpp
@@ -15,37 +15,55 @@ class Node {
 class Solution {
   public:
     
-    bool isHeap(Node* root) {
+    bool checkcondition2(Node *root) {
         
         if(!root)
         return true;
-        
-        if(!root->left && !root->right)
-        return true;
-        
-        if(!root->left && root->right)
-        return false;
-        
-        if(root->left && root->left->left) {
-            if(!root->left->right && root->right && root->right->left) {
-                return false;
-            }
-            if(!root->left->right && root->right && root->right->right) {
-                return false;
-            }
-        }
-        
         
         if(root->left) {
             if(root->data < root->left->data)
             return false;
         }
-        
         if(root->right) {
             if(root->data < root->right->data)
             return false;
         }
         
-        return isHeap(root->left) && isHeap(root->right);
+        return checkcondition2(root->left) && checkcondition2(root->right);
+    }
+    
+    int countnodes(Node *root) {
+        
+        if(!root)
+        return 0;
+        
+        return 1 + countnodes(root->left) + countnodes(root->right);
+    }
+    
+    bool traverse(Node *root, int &nodes, int i) {
+        
+        if(!root)
+        return true;
+        
+        if(i > nodes-1)
+        return false;
+        
+        return traverse(root->left,nodes,2*i+1) &&  traverse(root->right,nodes,2*i+2);
+    }
+    
+    bool checkcondition1(Node *root) {
+        
+        int nodes = countnodes(root);
+        int i = 0;
+        
+        return traverse(root,nodes,i);
+    }
+    
+    bool isHeap(Node* root) {
+        
+        // conditions for binary tree to be heap : 
+        // 1. it must be complete binary tree (no right child if dont have left child)
+        // 2. parent >= child data for every hierarchy
+        return checkcondition1(root) && checkcondition2(root);
     }
 };
