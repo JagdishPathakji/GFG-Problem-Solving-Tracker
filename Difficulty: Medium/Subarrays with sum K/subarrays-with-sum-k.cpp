afffec7c0c3ffcaa44df2ad72d8d9d75b974ea96
Jagdish Pathakji <jagdishgithub1@gmail.com>
2026-02-20 16:44:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subarrays with sum K/subarrays-with-sum-k.cpp
+++ Difficulty: Medium/Subarrays with sum K/subarrays-with-sum-k.cpp
@@ -5,13 +5,11 @@ class Solution {
         int prefixSum = 0;
         int ans = 0;
         unordered_map<int,int> mp;
+        mp[0] = 1;
         
         for(int i=0; i<arr.size(); i++) {
             
             prefixSum += arr[i];
-            
-            if(prefixSum == k)
-            ans += 1;
             
             if(mp.find(prefixSum-k) != mp.end())
             ans += mp[prefixSum-k];
