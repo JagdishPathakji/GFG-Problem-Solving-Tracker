6f37491c7e308bdaf5329a2caf2455c033fa052b
Jagdish Pathakji <jagdishgithub1@gmail.com>
2026-03-02 23:53:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Smaller Than X/count-smaller-than-x.cpp
@@ -0,0 +1,19 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    // arr[]: input array
+    // n: size of array
+    // x: element for which you need to find smaller than x
+    int smallerThanX(int arr[], int n, int x) {
+        
+        int cnt = 0;
+        
+        for(int i=0; i<n; i++) {
+            if(arr[i] < x)
+            cnt++;
+        }
+        
+        return cnt;
+    }
+};
