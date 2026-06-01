64a39a9dc726d2fa0ceb61756e95bef18be8e4f6
Jagdish Pathakji <jagdishgithub1@gmail.com>
2026-06-01 13:38:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Floyd Warshall/floyd-warshall.cpp
+++ Difficulty: Medium/Floyd Warshall/floyd-warshall.cpp
@@ -2,18 +2,20 @@
 
 class Solution {
   public:
-    void floydWarshall(vector<vector<int>> &dist) {
+    void floydWarshall(vector<vector<int>> &distance) {
         // Code here
-        // if any of the diagonal element becames -ve that means an -ve cycle is present.
-        int n = dist.size();
-        for(int k = 0; k<n; k++) {
+        
+        
+        int n = distance.size();
+        for(int k=0; k<n; k++) {
             for(int i=0; i<n; i++) {
                 for(int j=0; j<n; j++) {
-                    if(dist[i][k] != 1e8 and dist[k][j] != 1e8)
-                    dist[i][j] = min(dist[i][j],dist[i][k]+dist[k][j]);
+                    if(distance[i][k] == 1e8 || distance[k][j] == 1e8)
+                    continue;
+                    distance[i][j] = min(distance[i][j],distance[i][k] + distance[k][j]);
                 }
-            }   
+            }
         }
-    
+
     }
 };
